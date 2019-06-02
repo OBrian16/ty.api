32ee7e9f89d6947f701e11c7bc648420dbb41690
OBrian16 <briwell16@gmail.com>
2019-06-02 19:36:47 +0300

    Log connection to DB

--- datastores/db.js
+++ datastores/db.js
@@ -1,12 +1,15 @@
-const dbPool = 'A pool of connections to your DB'
+const dbPool = 'MongoDB Connected Succefully'
 
 const bcrypt = require('bcrypt')
 const mongo = require('mongoose')
 const env = require('dotenv').config().parsed
 
-mongo.connect(env.mongoDB, { useNewUrlParser: true });
+mongo.connect(env.mongoDB, { useNewUrlParser: true }).then(
+    () => { console.log(dbPool)},
+    err => { console.log('Can not connect to the database' + err) }
+);
 
 module.exports = {
-  dbPool,
-  mongo,
+    dbPool,
+    mongo,
 }
